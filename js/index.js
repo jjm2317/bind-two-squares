56d3a8474025fb9d65502fa346c6150b72ad1530
jjm2317 <wlaks2317@gmail.com>
2021-05-30 23:30:43 +0900

    fix: Create func removing events
    다른 box 클릭시 기존 box에 mousemove이벤트가 남아있는 버그 수정

--- js/index.js
+++ js/index.js
@@ -18,15 +18,23 @@ const offsets = [
   },
 ];
 const moveBox = (e, offset) => {
+  console.log(offset.x, offset.y);
   offset.x = e.clientX - startPos.x;
   offset.y = e.clientY - startPos.y;
 
   e.target.style.transform = `translate3d(${offset.x}px, ${offset.y}px, 1px )`;
 };
+const removeEventInAllNodes = (iterable, eventName) =>
+  [...iterable].forEach((node) => (node[eventName] = null));
+
 $container.onmousedown = (e) => {
   if (!e.target.matches(".box")) return;
+
   e.target.style.zIndex = 99;
   console.log(e.target.id);
+  /*다른 box 클릭시 기존 box에 mousemove이벤트가 남아있는 버그 수정*/
+  removeEventInAllNodes(e.currentTarget.children, "onmousemove");
+
   const offset = offsets.find(({ id }) => id === e.target.id);
   console.log(111);
   startPos.x = e.clientX - offset.x;
@@ -39,4 +47,9 @@ $container.onmouseup = (e) => {
   console.log(e.target.id);
   e.target.onmousemove = null;
   // document.removeEventListener("mousemove", moveBox);
+};
+
+$container.onmouseout = (e) => {
+  if (!e.target.matches(".box")) return;
+  // e.target.onmousemove = null;
 };
