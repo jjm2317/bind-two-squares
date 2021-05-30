ba5ee3eaa2c2c9f275d54a8edc16606a8f033087
jjm2317 <wlaks2317@gmail.com>
2021-05-31 00:54:18 +0900

    refactor: Remove trash codes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const $container = document.getElementById("container");
-console.log($container);
 
 const startPos = {
   x: 0,
@@ -17,39 +16,50 @@ const offsets = [
     y: 0,
   },
 ];
+
+// function
 const moveBox = (e, offset) => {
-  console.log(offset.x, offset.y);
+  // const { top, left } = e.target.getBoundingClientRect();
+  // if (top <= 0 || left <= 0) {
+  //   if (top <= 0) offset.y -= top;
+  //   if (left <= 0) offset.x -= left;
+  //   e.target.style.transform = `translate3d(${offset.x}px, ${offset.y}px, 0 )`;
+  //   return;
+  // }
+
   offset.x = e.clientX - startPos.x;
   offset.y = e.clientY - startPos.y;
 
-  e.target.style.transform = `translate3d(${offset.x}px, ${offset.y}px, 1px )`;
+  e.target.style.transform = `translate3d(${offset.x}px, ${offset.y}px, 0 )`;
 };
-const removeEventInAllNodes = (iterable, eventName) =>
-  [...iterable].forEach((node) => (node[eventName] = null));
 
-$container.onmousedown = (e) => {
-  if (!e.target.matches(".box")) return;
-
+const settingBoxMousedown = (e) => {
   e.target.style.zIndex = 99;
   console.log(e.target.id);
   /*다른 box 클릭시 기존 box에 mousemove이벤트가 남아있는 버그 수정*/
   removeEventInAllNodes(e.currentTarget.children, "onmousemove");
 
   const offset = offsets.find(({ id }) => id === e.target.id);
-  console.log(111);
+
   startPos.x = e.clientX - offset.x;
   startPos.y = e.clientY - offset.y;
   e.target.onmousemove = (e) => moveBox(e, offset);
 };
+
+const settingPointMousedown = (e) => {};
+
+const removeEventInAllNodes = (iterable, eventName) =>
+  [...iterable].forEach((node) => (node[eventName] = null));
+
+//event handler
+$container.onmousedown = (e) => {
+  if (e.target.matches(".box")) settingBoxMousedown(e);
+  if (e.target.matches(".point")) settingPointMousedown(e);
+};
+
 $container.onmouseup = (e) => {
   if (!e.target.matches(".box")) return;
   e.target.style.zIndex = 0;
   console.log(e.target.id);
   e.target.onmousemove = null;
-  // document.removeEventListener("mousemove", moveBox);
-};
-
-$container.onmouseout = (e) => {
-  if (!e.target.matches(".box")) return;
-  // e.target.onmousemove = null;
 };
